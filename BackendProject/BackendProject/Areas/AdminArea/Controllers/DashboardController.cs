83cfa16b01107d23aaaf9f12969e57b96ed33186
ismayilzeynal <ismayilbz@code.edu.az>
2023-03-28 15:30:08 +0400

    feature: adminarea created

---
+++ BackendProject/BackendProject/Areas/AdminArea/Controllers/DashboardController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendProject.Areas.AdminArea.Controllers
+{
+    [Area("AdminArea")]
+    //[Authorize(Roles = "Admin")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- BackendProject/BackendProject/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BackendProject.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
