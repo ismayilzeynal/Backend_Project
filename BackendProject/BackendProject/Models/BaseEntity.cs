6a8c744b5616f0eec3b7926cae222032412d8eb1
ismayilzeynal <ismayilbz@code.edu.az>
2023-03-29 03:25:36 +0400

    feature: slider model created

---
+++ BackendProject/BackendProject/Models/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace BackendProject.Models
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ BackendProject/BackendProject/Models/Slider.cs
@@ -0,0 +1,9 @@
+﻿namespace BackendProject.Models
+{
+    public class Slider : BaseEntity
+    {
+        public string Title { get; set; }
+        public string Desc { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
