16c1677dc16240fe52c75c2cc0e6ba6c980265c1
ismayilzeynal <ismayilbz@code.edu.az>
2023-04-04 20:25:50 +0400

    feature: comment on blog page

--- BackendProject/BackendProject/Controllers/BlogController.cs
+++ BackendProject/BackendProject/Controllers/BlogController.cs
@@ -1,6 +1,8 @@
 ﻿using BackendProject.DAL;
+using BackendProject.Models;
 using BackendProject.ViewModels.Blog;
 using BackendProject.ViewModels.Course;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,10 +11,12 @@ namespace BackendProject.Controllers
     public class BlogController : Controller
     {
         private readonly AppDbContext _appDbContext;
+        private readonly UserManager<AppUser> _userManager;
 
-        public BlogController(AppDbContext appDbContext)
+        public BlogController(AppDbContext appDbContext, UserManager<AppUser> userManager)
         {
             _appDbContext = appDbContext;
+            _userManager = userManager;
         }
         public IActionResult Index()
         {
@@ -35,11 +39,23 @@ namespace BackendProject.Controllers
 
             return View(blogVMs);
         }
-        public IActionResult Detail(int id)
+        public async Task<IActionResult> Detail(int id)
         {
             if(id == null) return NotFound();
+
+
+            ViewBag.UserId = null;
+            if (User.Identity.IsAuthenticated)
+            {
+                AppUser user = await _userManager.FindByNameAsync(User.Identity.Name);
+                ViewBag.UserId = user.Id;
+            }
+
             BlogDetailVM blogDetailVM= new();
-            blogDetailVM.Blog = _appDbContext.Blogs.FirstOrDefault(b => b.Id == id);
+            blogDetailVM.Blog = _appDbContext.Blogs
+                .Include(bm => bm.Comments)
+                .ThenInclude(c => c.AppUser)
+                .FirstOrDefault(b => b.Id == id);
             blogDetailVM.Categories = _appDbContext.Categories
                 .Include(c=>c.Courses)
                 .Take(6)
@@ -53,5 +69,50 @@ namespace BackendProject.Controllers
 
             return View(blogDetailVM);
         }
+
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> AddComment(string commentMessage, int blogId)
+        {
+            AppUser user = null;
+
+            if (User.Identity.IsAuthenticated)
+            {
+                user = await _userManager.FindByNameAsync(User.Identity.Name);
+                ViewBag.UserId = user.Id;
+            }
+            else
+            {
+                return RedirectToAction("login", "account");
+            }
+
+            Comment comment = new();
+            comment.Message = commentMessage;
+            comment.CreatedDate = DateTime.Now;
+            comment.AppUserId = user.Id;
+            comment.BlogId = blogId;
+            _appDbContext.Comments.Add(comment);
+            _appDbContext.SaveChanges();
+            return RedirectToAction("Detail", new { id = blogId });
+        }
+
+        public async Task<IActionResult> DeleteComment(int id)
+        {
+            var comment = _appDbContext.Comments.FirstOrDefault(b => b.Id == id);
+
+            _appDbContext.Comments.Remove(comment);
+            _appDbContext.SaveChanges();
+            return RedirectToAction("detail", new { id = comment.BlogId });
+        }
+
+
+
+
+
+
+
+
+
     }
 }
