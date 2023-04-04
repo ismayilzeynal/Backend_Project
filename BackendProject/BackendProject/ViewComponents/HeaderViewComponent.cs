00e7c97d876f5e0814b063ff730dcd3ad2c4becf
ismayilzeynal <ismayilbz@code.edu.az>
2023-04-04 05:49:49 +0400

    username show at header

--- BackendProject/BackendProject/ViewComponents/HeaderViewComponent.cs
+++ BackendProject/BackendProject/ViewComponents/HeaderViewComponent.cs
@@ -1,5 +1,6 @@
 ﻿using BackendProject.DAL;
 using BackendProject.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BackendProject.ViewComponents
@@ -8,14 +9,22 @@ namespace BackendProject.ViewComponents
     {
 
         private readonly AppDbContext _appDbContext;
+        private readonly UserManager<AppUser> _userManager;
 
-        public HeaderViewComponent(AppDbContext appDbContext)
+        public HeaderViewComponent(AppDbContext appDbContext, UserManager<AppUser> userManager)
         {
             _appDbContext = appDbContext;
+            _userManager = userManager;
         }
 
         public async Task<IViewComponentResult> InvokeAsync()
         {
+            ViewBag.FullName = string.Empty;
+            if (User.Identity.IsAuthenticated)
+            {
+                AppUser user = await _userManager.FindByNameAsync(User.Identity.Name);
+                ViewBag.FullName = user.Fullname;
+            }
             Bio bio = _appDbContext.Bios.FirstOrDefault();
             return View(await Task.FromResult(bio));
         }
