beaf776f8c2c2c8999b7afef11a03e5a2e582384
ismayilzeynal <ismayilbz@code.edu.az>
2023-04-05 07:40:09 +0400

    feature: adminPanel navbar put inside component

---
+++ BackendProject/BackendProject/Areas/AdminArea/ViewComponents/AdminHeaderViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using BackendProject.DAL;
+using BackendProject.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendProject.ViewComponents
+{
+    public class AdminHeaderViewComponent : ViewComponent
+    {
+
+        private readonly AppDbContext _appDbContext;
+        private readonly UserManager<AppUser> _userManager;
+
+        public AdminHeaderViewComponent(AppDbContext appDbContext, UserManager<AppUser> userManager)
+        {
+            _appDbContext = appDbContext;
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            ViewBag.FullName = string.Empty;
+            if (User.Identity.IsAuthenticated)
+            {
+                AppUser user = await _userManager.FindByNameAsync(User.Identity.Name);
+                ViewBag.FullName = user.Fullname;
+            }
+            Bio bio = _appDbContext.Bios.FirstOrDefault();
+            return View(await Task.FromResult(bio));
+        }
+    }
+}
